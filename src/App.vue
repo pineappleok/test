2c95ec6bfa5ef97880a464ad7d233300a074e7c1
pineappleok <550185271@qq.com>
2018-08-16 20:19:40 +0800

    1、package.json为cnpm install mint-ui --save的痕迹；src/main.js全部引入mint-ui及样式并use，src/App.vue页面使用mint-ui的组件，button组件不需要另外引入，但是toast组件需要另外import { Toast }；

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <mt-button type="default" @click.native="toast">default</mt-button>
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+import { Toast } from 'mint-ui';
+  export default {
+    name: 'App',
+    methods: {
+      toast() {
+        Toast({
+          message: '提示',
+          position: 'bottom',
+          duration: 5000
+        });
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
